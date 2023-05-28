c9812c4c40074f70902ac761007532a99cc289ec
dearlsh94 <dearlsh94@gmail.com>
2023-05-28 19:48:16 +0900

    Add : Star Component

---
+++ src/components/ui/Star.tsx
@@ -0,0 +1,26 @@
+export interface IStar {
+  positionTop: string
+  positionLeft: string
+  size: number
+  color?: string
+  className?: string
+}
+
+const Star = ({ positionTop, positionLeft, size, color, className }: IStar) => {
+  console.log({ color })
+  return (
+    <div
+      className={`star ${className}`}
+      style={{
+        top: positionTop,
+        left: positionLeft,
+        width: `${size}px`,
+        height: `${size}px`,
+        boxShadow: `0px 0px ${size * 1.5}px #fff`,
+        backgroundColor: `${color || '#fff'}`,
+      }}
+    />
+  )
+}
+
+export default Star
