601f3b958595f811d69b6e48dce01c1277c622f2
dearlsh94 <dearlsh94@gmail.com>
2023-10-02 22:04:15 +0900

    Modify : star count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ interface IHome {}
 
 const Index: NextPage<IHome> = () => {
   const [stars, setStars] = useState<IStar[]>([])
-  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   const createRandomStar = (): IStar => {
     return {
@@ -22,22 +21,13 @@ const Index: NextPage<IHome> = () => {
     }
   }
 
-  const handleMouseMove = e => {
-    setPosition({ x: e.clientX, y: e.clientY })
-  }
-
   useEffect(() => {
     starRefresh()
-
-    window.addEventListener('mousemove', handleMouseMove)
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
-    }
   }, [])
 
   const starRefresh = () => {
     const s = []
-    for (let i = 0; i < 70; i++) {
+    for (let i = 0; i < 9; i++) {
       s.push(createRandomStar())
     }
     setStars(s)
@@ -48,26 +38,19 @@ const Index: NextPage<IHome> = () => {
       <Head>
         <title>Treefeely</title>
       </Head>
-      <main className="home-container" onMouseMove={handleMouseMove}>
+      <main className="home-container">
         <div className="circle-text-box" onClick={starRefresh}>
-          <CircleText text="TREEFEELY TREEFEELY TREEFEELY" size={'300px'} />
+          <CircleText text="TREEFEELY" size={'300px'} />
         </div>
-        <a className="weezip-line" href="https://weezip.treefeely.com" target="_blank" rel="noreferrer">
+        <a
+          className="weezip-line"
+          href="https://weezip.treefeely.com"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Weezip으로 이동"
+        >
           <LineText text="WEEZIP" />
         </a>
-        {/* {stars.map((star, i) => (
-          <Star
-            key={`star-${i}`}
-            className={`star-${i}`}
-            positionTop={star.positionTop}
-            positionLeft={star.positionLeft}
-            size={star.size}
-            color={star.color}
-            twinkleDuration={star.twinkleDuration}
-            moveX={position.x}
-            moveY={position.y}
-          />
-        ))} */}
         {stars.map((star, i) => (
           <Star
             key={`star-${i}`}
