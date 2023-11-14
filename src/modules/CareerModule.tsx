1c12c0326b7dfb7f137388d7d4ed77b777a6a4ae
dearlsh94 <dearlsh94@gmail.com>
2023-11-14 16:40:32 +0900

    Modify : Text

--- src/modules/CareerModule.tsx
+++ src/modules/CareerModule.tsx
@@ -9,7 +9,7 @@ import Head2Layout from '@/components/layouts/Head2Layout'
 export default function CareerModule() {
   return (
     <FlexView direction="column">
-      <Head2Layout text={'저는 회사에서 이런 일들을 했어요.'} />
+      <Head2Layout text={'회사에서 이런 일들을 했어요.'} />
       <AOSFullCardLayout type="fade-right" align="flex-start" mt={8}>
         <Baropharm />
       </AOSFullCardLayout>
