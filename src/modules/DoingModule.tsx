b7931b37a1b19ed894a4a07647359646a0d76b93
dearlsh94 <dearlsh94@gmail.com>
2023-11-21 12:22:19 +0900

    Modify : DoingModule Content

--- src/modules/DoingModule.tsx
+++ src/modules/DoingModule.tsx
@@ -9,36 +9,45 @@ export default function DoingModule() {
       <Head2Layout text={'이런 활동들을 하고 있어요.'} />
       <AOSHalfLayout type="flip-left">
         <Text textAlign="center">
-          프론트엔드다이빙클럽
-          <br /> 멤버
+          글쓰는 개발자 모임
+          <br />
+          글또 9기
         </Text>
         2023.11 ~
       </AOSHalfLayout>
       <AOSHalfLayout type="flip-right">
         <Text textAlign="center">
+          프론트엔드
+          <br />
+          다이빙 클럽
+        </Text>
+        2023.11 ~
+      </AOSHalfLayout>
+      <AOSHalfLayout type="flip-left">
+        <Text textAlign="center">
           재고 관리 서비스
-          <br /> 안녕재고 팀원
+          <br /> 안녕재고
         </Text>
         2023.10 ~
       </AOSHalfLayout>
-      <AOSHalfLayout type="flip-left">
+      <AOSHalfLayout type="flip-right">
         <Text textAlign="center">
           개인 블로그
-          <br /> Weezip 운영
+          <br /> Weezip
         </Text>
         2023.04 ~
       </AOSHalfLayout>
-      <AOSHalfLayout type="flip-right">
+      <AOSHalfLayout type="flip-left">
         <Text textAlign="center">
           영화 및 독서모임
-          <br /> 북이영화 멤버
+          <br /> 북이영화
         </Text>
         2021.06 ~
       </AOSHalfLayout>
-      <AOSHalfLayout type="flip-left">
+      <AOSHalfLayout type="flip-right">
         <Text textAlign="center">
           클럽하우스 음악모임
-          <br /> 검치단 단원
+          <br /> 검치단
         </Text>
         2021.03 ~
       </AOSHalfLayout>
