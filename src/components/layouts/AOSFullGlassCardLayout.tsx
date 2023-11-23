04f73307f9be34a8456967fe8b5456cc8a86fe87
dearlsh94 <dearlsh94@gmail.com>
2023-11-23 12:10:21 +0900

    Style, Refactor : Glassmorphism

--- src/components/layouts/AOSFullGlassCardLayout.tsx
+++ src/components/layouts/AOSFullGlassCardLayout.tsx
@@ -1,18 +1,18 @@
 import { css } from '@emotion/react'
 
-interface AOSFullCardLayoutProps {
-  type?: 'fade-down' | 'fade-right' | 'flip-up'
+interface AOSFullGlassCardLayoutProps {
+  type?: 'fade-down' | 'flip-up'
   align?: 'flex-start' | 'center' | 'flex-end'
   mt?: number
   children: React.ReactNode
 }
 
-export default function AOSFullCardLayout({
+export default function AOSFullGlassCardLayout({
   type = 'fade-down',
   align = 'center',
   mt = 6,
   children,
-}: AOSFullCardLayoutProps) {
+}: AOSFullGlassCardLayoutProps) {
   return (
     <div
       data-aos={type}
@@ -21,7 +21,8 @@ export default function AOSFullCardLayout({
         max-width: 1024px;
         clear: both;
         min-height: 120px;
-        background: #fff;
+        backdrop-filter: blur(16px) saturate(180%);
+        -webkit-backdrop-filter: blur(16px) saturate(180%);
         border-radius: 8px;
         margin: ${`${mt}vh 0`};
         padding: 16px 12px;
@@ -31,9 +32,9 @@ export default function AOSFullCardLayout({
         flex-direction: column;
         row-gap: 8px;
         box-sizing: border-box;
-        box-shadow: ${`${type === 'fade-right' ? '8px' : '0px'} 8px 12px 0px rgba(0, 0, 0, 0.65)`};
-        -webkit-box-shadow: ${`${type === 'fade-right' ? '8px' : '0px'} 8px 12px 0px rgba(0, 0, 0, 0.65)`};
-        -moz-box-shadow: ${`${type === 'fade-right' ? '8px' : '0px'} 8px 12px 0px rgba(0, 0, 0, 0.65)`};
+        box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.35);
+        -webkit-box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.35);
+        -moz-box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.35);
       `}
     >
       {children}
