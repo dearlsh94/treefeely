10487ef437277846301dbbb3e11a842483612e3f
dearlsh94 <dearlsh94@gmail.com>
2023-12-17 13:00:39 +0900

    Modify : 현재 활동 수정 및 링크 추가

--- src/modules/DoingModule.tsx
+++ src/modules/DoingModule.tsx
@@ -2,16 +2,43 @@ import FlexView from '@/components/common/FlexView'
 import AOSHalfLayout from '@/components/layouts/AOSHalfLayout'
 import Head2Layout from '@/components/layouts/Head2Layout'
 import Text from '@/components/common/Text'
+import { IconNotion } from '@/components/icons/IconNotion'
+import { css } from '@emotion/react'
+import IconLinkOut from '@/components/icons/IconLinkOut'
 
 export default function DoingModule() {
   return (
     <FlexView direction="column">
       <Head2Layout text={'이런 활동들을 하고 있어요.'} />
-      <AOSHalfLayout type="flip-left">
+      <AOSHalfLayout type="flip-right">
+        <a
+          href={'https://www.notion.so/ac5b18a482fb4df497d4e8257ad4d516'}
+          target="_blank"
+          aria-label={`글또 노션 페이지 이동`}
+          rel="noopener noreferrer"
+          css={css`
+            width: 24px;
+            height: 24px;
+          `}
+        >
+          <IconNotion />
+        </a>
         <Text textAlign="center">글또 9기</Text>
         2023.11 ~
       </AOSHalfLayout>
-      <AOSHalfLayout type="flip-right">
+      <AOSHalfLayout type="flip-left">
+        <a
+          href={'https://toss.tech/article/frontend-diving-club'}
+          target="_blank"
+          aria-label={`프론트엔드 다이빙 클럽 소개 페이지 이동`}
+          rel="noopener noreferrer"
+          css={css`
+            width: 18px;
+            height: 18px;
+          `}
+        >
+          <IconLinkOut size={18} />
+        </a>
         <Text textAlign="center">
           프론트엔드
           <br />
@@ -19,14 +46,19 @@ export default function DoingModule() {
         </Text>
         2023.11 ~
       </AOSHalfLayout>
-      <AOSHalfLayout type="flip-left">
-        <Text textAlign="center">
-          개인 블로그
-          <br /> Weezip
-        </Text>
-        2023.04 ~
-      </AOSHalfLayout>
       <AOSHalfLayout type="flip-right">
+        <a
+          href={'https://treefeely.notion.site/treefeely/2543fb95b1564550b3e8dcfdf4d63a1b'}
+          target="_blank"
+          aria-label={`북이영화 노션 페이지 이동`}
+          rel="noopener noreferrer"
+          css={css`
+            width: 24px;
+            height: 24px;
+          `}
+        >
+          <IconNotion />
+        </a>
         <Text textAlign="center">
           영화 및 독서모임
           <br /> 북이영화
